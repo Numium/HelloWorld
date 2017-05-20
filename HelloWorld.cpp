a89ecf64d1255e660d73ec2340f80b665970b614
Numium <stefan_blochberger@gmx.de>
2017-05-20 13:12:27 +0200

    Add files via upload

---
+++ HelloWorld.cpp
@@ -0,0 +1,14 @@
+// HelloWorld.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	cout << "Hello World" << endl;
+	cout << "Here is VS2015" << endl;
+    return 0;
+}
+
